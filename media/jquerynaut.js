5748b9c1aed6d856f4fd56acf206b178073c2b64
Nikolay Rekubratskiy <oruen@undev.ru>
2009-03-30 18:42:32 +0400

    Fixed reconnect function for jQuery.

--- media/jquerynaut.js
+++ media/jquerynaut.js
@@ -3,28 +3,55 @@
 
 Juggernaut.fn.fire_event = function(fx_name) {
      $(document).trigger("juggernaut:" + fx_name);
-   }
+   };
 
 Juggernaut.fn.bindToWindow = function() {
     $(window).bind("load", this, function(e) {
       juggernaut = e.data;
-      e.data.appendFlashObject()
+      e.data.appendFlashObject();
     });
-  }
+  };
 
 Juggernaut.toJSON = function(hash) {
     return $.toJSON(hash) ;
-  } 
+  };
 
 Juggernaut.parseJSON = function(string) {
     return $.parseJSON(string);
-  }
+  };
 
 Juggernaut.fn.swf = function(){
     return $('#' + this.options.swf_name)[0];
-  }
+  };
   
 Juggernaut.fn.appendElement = function() {
     this.element = $('<div id=juggernaut>');
     $("body").append(this.element);
-  }
+  };
+
+Juggernaut.fn.refreshFlashObject = function(){
+    $(this.swf()).remove();
+    this.appendFlashObject();
+  };
+
+Juggernaut.fn.reconnect = function(){
+    if(this.options.reconnect_attempts){
+      this.attempting_to_reconnect = true;
+      this.fire_event('reconnect');
+      this.logger('Will attempt to reconnect ' + this.options.reconnect_attempts + ' times, the first in ' + (this.options.reconnect_intervals || 3) + ' seconds');
+      var self = this;
+      for(var i=0; i < this.options.reconnect_attempts; i++){
+        setTimeout(function(){
+          if(!self.is_connected){
+            self.logger('Attempting reconnect');
+            if(!self.ever_been_connected){
+              self.refreshFlashObject();
+            } else {
+              self.connect();
+            }
+          }
+        }, (this.options.reconnect_intervals || 3) * 1000 * (i + 1));
+        
+      }
+    }
+  };
