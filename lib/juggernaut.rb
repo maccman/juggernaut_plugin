0bea7ccbe56cc1d4576a3c288738aeadc34bb01d
Alex MacCaw <maccman@gmail.com>
2009-08-03 17:02:53 +0100

    Merge Ruby 1.9 fixes

--- lib/juggernaut.rb
+++ lib/juggernaut.rb
@@ -184,31 +184,17 @@ module Juggernaut
             when :send_to_clients
               juggernaut_needs options, :client_ids
               Juggernaut.send_to_clients(data, options[:client_ids])
-<<<<<<< HEAD:lib/juggernaut.rb
-            when :send_to_client_on_channel:
+            when :send_to_client_on_channel
               juggernaut_needs options, :client_id, :channel
               Juggernaut.send_to_clients_on_channel(data, options[:client_id], options[:channel])
-            when :send_to_clients_on_channel:
-              juggernaut_needs options, :client_ids, :channel
-              Juggernaut.send_to_clients_on_channel(data, options[:client_ids], options[:channel])
-            when :send_to_client_on_channels:
-              juggernaut_needs options, :client_ids, :channels
-              Juggernaut.send_to_clients_on_channel(data, options[:client_id], options[:channels])
-            when :send_to_clients_on_channels:
-              juggernaut_needs options, :client_ids, :channels
-=======
-            when :send_to_client_on_channel
-              juggernaut_needs options, :client_id, :channels
-              Juggernaut.send_to_clients_on_channel(data, options[:client_id], options[:channels])
             when :send_to_clients_on_channel
               juggernaut_needs options, :client_ids, :channel
               Juggernaut.send_to_clients_on_channel(data, options[:client_ids], options[:channel])
             when :send_to_client_on_channels
-              juggernaut_needs options, :client_ids, :channel
+              juggernaut_needs options, :client_ids, :channels
               Juggernaut.send_to_clients_on_channel(data, options[:client_id], options[:channels])
             when :send_to_clients_on_channels
-              juggernaut_needs options, :client_ids, :channel
->>>>>>> aa44ad35759661d751c7db114fccb85fadb71514:lib/juggernaut.rb
+              juggernaut_needs options, :client_ids, :channels
               Juggernaut.send_to_clients_on_channel(data, options[:client_ids], options[:channels])
           end
         end
